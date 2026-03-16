24094421a19a55e99c6730ee047159f53717340e
kauancordeiro <cvitorcordeiro@gmail.com>
2026-03-15 22:55:08 -0300

    feat: add encoded password

--- src/main/java/com/example/auth_server/user/model/User.java
+++ src/main/java/com/example/auth_server/user/model/User.java
@@ -3,11 +3,17 @@ package com.example.auth_server.user.model;
 import com.example.auth_server.user.dto.CreateUserRequest;
 import com.example.auth_server.user.model.enums.Role;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Users")
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,10 +27,10 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    public User(CreateUserRequest request) {
+    public User(CreateUserRequest request, String passEncoded) {
         this.setName(request.getName());
         this.setEmail(request.getEmail());
-        this.setPassword(request.getPassword());
+        this.setPassword(passEncoded);
         this.setRole(Role.USER);
     }
 }
--- src/main/java/com/example/auth_server/user/service/UserService.java
+++ src/main/java/com/example/auth_server/user/service/UserService.java
@@ -5,6 +5,8 @@ import com.example.auth_server.user.dto.UserResponse;
 import com.example.auth_server.user.model.User;
 import com.example.auth_server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,13 +17,19 @@ public class UserService {
 
     public UserResponse createUser(CreateUserRequest request) {
         validateEmail(request.getEmail());
-        User user = new User(request);
+        String passEncoded = encodePass(request.getPassword());
+        User user = new User(request, passEncoded);
         user = userRepository.save(user);
         return new UserResponse(user);
     }
 
+    private String encodePass(String password) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.encode(password);
+    }
+
     private void validateEmail(String email) {
-        if(userRepository.existsByEmail(email)) {
+        if (userRepository.existsByEmail(email)) {
             throw new RuntimeException("Email already registered");
         }
     }
